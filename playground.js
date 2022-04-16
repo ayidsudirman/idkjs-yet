b172f8217924fb8d8e6c1c4ca113229955a24d6a
ayidsudirman <ayidsudirman@protonmail.com>
2022-04-17 04:12:01 +0800

    changing text & html, nodes, and DOM traversing

--- playground.js
+++ playground.js
@@ -1,11 +1,11 @@
-// Get Element By ID
+// GET ELEMENT BY ID
 var banner = document.getElementById('page-banner');
 // console.info(banner);
 
 var bookList = document.getElementById('book-list');
 // console.log(bookList);
 
-// Get Element By Class or Tag
+// GET ELEMENT BY CLASS OR TAG
 var titles = document.getElementsByClassName('title'); 
 // console.log(titles);
 
@@ -30,3 +30,44 @@ var lis = document.getElementsByTagName('li');
 for (i=0; i < lis.length; i++) { 
     // console.log(lis[i]);
 } // also to cycle HTML Collection, but forEach is prefer one
+
+// THE QUERY SELECTOR
+var book = document.querySelector('#book-list li:nth-child(2) .name');
+// console.log(book); // always return html collection
+
+var books = document.querySelectorAll('#book-list li');
+// console.log(books); // always return node-list, do not need array method
+
+// books.forEach(book => console.log(book));
+
+// CHANGING TEXT AND HTML CONTENT
+books = document.querySelectorAll('#book-list li .name');
+books.forEach((book => book.textContent += ' (judul kitab)'))
+
+bookList = document.querySelector('#book-list');
+// bookList.innerHTML = '<h2>Kitab Mafatihul Ilmi</h2>';
+bookList.innerHTML += '<p>Kitab Mafatihul Ilmi selanjutnya ...</p>';
+
+// NODES
+// banner = document.querySelector('#page-banner');
+// console.log('#banner node type is:', banner.nodeType);
+// console.log('#banner node name is:', banner.nodeName);
+// console.log('#banner node has child:', banner.hasChildNodes());
+
+const clonedBanner = banner.cloneNode(false);
+console.log(clonedBanner);
+
+// TRAVERSING THE DOM
+// console.log('Banner parent node is:', banner.parentNode);
+// console.log('Banner parent element is:', banner.parentElement);
+// console.log('Banner top-level parent element is:', banner.parentElement.parentElement);
+// console.log('book-list children is:', bookList.children);
+
+// console.log('book-list next sibling is: ', bookList.nextSibling); // line break is also a text
+// console.log('book-list next element sibling is: ', bookList.nextElementSibling); 
+// console.log('book-list next previous sibling is: ', bookList.previousSibling);
+// console.log('book-list next previous element sibling is: ', bookList.previousElementSibling); 
+
+bookList.previousElementSibling.querySelector('p').innerHTML += '<br /> yang wajib diketahui setiap muslim dan muslimah';
+
+
