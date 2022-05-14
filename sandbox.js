ad066010d97f37b9b60c96d945c0f4ef32b1bf8f
ayidsudirman <ayidsudirman@protonmail.com>
2022-05-14 23:04:54 +0800

    HTTP Request

--- sandbox.js
+++ sandbox.js
@@ -1,6 +1,20 @@
-console.log('1');
-setTimeout(() => {
-    console.log('Callback function fired');
-}, 2000)
-console.log('2');
-console.log('3');
+// ASNYCHRONOUS VS SYNCHRONOUS
+	// console.log('1');
+	// setTimeout(() => {
+	//     console.log('Callback function fired');
+	// }, 2000)
+	// console.log('2');
+	// console.log('3');
+
+// HTTP REQUEST
+const request = new XMLHttpRequest();
+
+request.addEventListener('readystatechange', () => {
+	// console.log(request, request.readyState);
+	if (request.readyState === 4 ) {
+		console.log(request.responseText)
+	}
+});
+
+request.open('GET', 'https://jsonplaceholder.typicode.com/todos');
+request.send();
